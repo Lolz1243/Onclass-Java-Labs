1b02cd67a91bb3ec7444d75620e49a0aef05a041
Nikhil Vytla <nvawesomeguy@gmail.com>
2018-09-22 23:20:28 -0400

    Day 11 & Lab 10

---
+++ Day 11 & Lab 10/CompilerError.java
@@ -0,0 +1,34 @@
+package P1118;
+
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+
+public class CompilerError
+{
+
+   public static void main(String[] args)
+   {
+
+         try //empty try block
+         {
+        	 
+         }
+         catch (Exception e)//SUPERCLASS Exception Type
+         {
+        	 
+         }
+         catch (InputMismatchException inputMismatchException)//SUBCLASS Exception Type
+         {
+        	 
+         }
+         catch (ArithmeticException arithmeticException)//SUBCLASS Exception Type
+         {
+        	 
+         }
+         catch (NoSuchElementException noSuchElementException)//SUBCLASS Exception Type
+         {
+        	 
+         }
+
+   } 
+}
